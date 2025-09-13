a29cdcdc78309b12ed62690a43a07a540a80a10f
Aniket Barai <aniketbarai677@gmail.com>
2025-09-14 01:39:40 +0530

    Update arrayList.java

--- ArraysJava/arrayList.java
+++ ArraysJava/arrayList.java
@@ -36,8 +36,20 @@ public class arrayList {
             for (int j = 0; j < 3; j++) {
                 list2D.get(i).add(sc.nextInt());
             }
-        }
         System.out.println(list2D);
     }
-
+     public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+    int count = 0;
+    for (int i = 0; i < ar.size(); i++) {
+        for (int j = i+1; j < ar.size(); j++) {
+            //this below statement checks the condition..
+            if((ar.get(i)+ar.get(j)) % k == 0){
+                count++; //when condition met increment by 1 
+            }
+        }
+    }
+    return count;
+    }
+        }
 }
+
